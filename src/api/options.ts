96680e34e3127c66e9ae4ed113b41cd0a6fa5ee9
Tigran Gabulyan <70902232+tik-217@users.noreply.github.com>
2024-03-02 18:17:15 +0300

    Update options.ts http to https

--- src/api/options.ts
+++ src/api/options.ts
@@ -2,7 +2,7 @@ import md5 from "md5";
 
 const date = new Date().toLocaleDateString().split(".").reverse().join("");
 
-export const url = "http://api.valantis.store:40000/";
+export const url = "https://api.valantis.store:41000/";
 
 export const headers = {
   "X-Auth": md5(`Valantis_${date}`),
